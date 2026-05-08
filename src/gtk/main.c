66266a6be79779a39c165fc482e5aa260ba2795b
Patrick Aljord <patcito@gmail.com>
2026-05-08 02:16:36 +0200

    Release 0.2.31

--- src/gtk/main.c
+++ src/gtk/main.c
@@ -49,7 +49,7 @@
 // ── Global state ──
 
 #ifndef PRETTYMUX_VERSION
-#define PRETTYMUX_VERSION "0.2.30"
+#define PRETTYMUX_VERSION "0.2.31"
 #endif
 
 static void terminal_search_send_action(GhosttyTerminal *term, const char *action);
